d5da75304a43891779c2fc30a3d59468799931fa
Filipe Cavalcante <filipe.khalifa@gmail.com>
2023-01-16 22:15:07 -0300

    Corrigir erros

--- persistencelayer/models/Bank.js
+++ persistencelayer/models/Bank.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 const Debt = require('./Debt')
 
 const BankSchema = new mongoose.Schema(
-        {
-		      _idUser: {
-                 type: mongoose.Schema.Types.ObjectId, 
-                 required: true
-              },
-            bankName: String,
-            debit: Debt[{}],
-            totalDebitAmount: Number,
-            accountValue: Number
-           },
-         { timestamps: true }
+  {
+    _idUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true
+    },
+    bankName: String,
+    debit: [{ decription: String, value: Number }],
+    totalDebitAmount: Number,
+    accountValue: Number
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model('Bank', BankSchema);
